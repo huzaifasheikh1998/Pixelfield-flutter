b1c366ffa847e42a6abb6ecd331490ef26f8eb64
huzaifasheikh1998 <huzaifasheikhupwork@gmail.com>
2025-02-11 18:01:58 +0500

    Add app fonts and constants for asset management and styling

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const black = Colors.black;
+  static const transparent = Colors.transparent;
+  static const white = Colors.white;
+}
